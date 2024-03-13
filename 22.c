507538ea3c069751afa9e35ef026c66c709c2ded
LucasSalvador_ <88511757+Lucasbar5@users.noreply.github.com>
2024-03-13 09:27:26 -0300

    Add files via upload

---
+++ 22.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () {
+    
+    printf("O valor de (A): %d\n ", 'A');
+    printf("O valor de (B): %d\n ", 'B');
+    printf("O valor de (C): %d\n ", 'C');
+    printf("O valor de (a): %d\n ", 'a');
+    printf("O valor de (b): %d\n ", 'b');
+    printf("O valor de (c): %d\n ", 'c');
+    printf("O valor de (0): %d\n ", '0');
+    printf("O valor de (1): %d\n ", '1');
+    printf("O valor de (2): %d\n ", '2');
+    printf("O valor de ($): %d\n ", '$');
+    printf("O valor de (*): %d\n ", '*');
+    printf("O valor de (+): %d\n ", '+');
+    printf("O valor de (/): %d\n ", '/');
+    printf("O valor de ( ): %d\n ", ' ');
+
+    return 0;
+}
